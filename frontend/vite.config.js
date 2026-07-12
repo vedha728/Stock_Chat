7ffde097563745196a9a384710778fca2cb5a502
Vedhaganesh K <vedhaganesh17@gmail.com>
2026-07-12 12:27:45 +0530

    Revert Vite output path to default frontend/dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
   server: {
     proxy: {
       '/api': {
